987b496906a1669de83da8968507c86d7474a501
maciejg-git <ender@localhost>
2022-11-23 01:21:55 +0100

    update

--- dist-bootstrap/icons/prescription2.js
+++ dist-bootstrap/icons/prescription2.js
@@ -10,7 +10,7 @@ export default {
   render() {
     return h(
       "svg",
-      {"xmlns":"http://www.w3.org/2000/svg","width":"16","height":"16","fill":"currentColor","viewBox":"0 0 16 16","data-name":"b-prescription2"},
+      {"xmlns":"http://www.w3.org/2000/svg","width":"16","height":"16","fill":"currentColor","viewBox":"0 0 16 16"},
       [ 
         h(
           "path",
@@ -18,7 +18,7 @@ export default {
         ),
         h(
           "path",
-          {"fill-rule":"evenodd","d":"M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v10.5a1.5 1.5 0 0 1-1.5 1.5h-7A1.5 1.5 0 0 1 3 14.5V4a1 1 0 0 1-1-1V1Zm2 3h8v10.5a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5V4ZM3 3V1h10v2H3Z"}
+          {"d":"M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v10.5a1.5 1.5 0 0 1-1.5 1.5h-7A1.5 1.5 0 0 1 3 14.5V4a1 1 0 0 1-1-1V1Zm2 3v10.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V4H4ZM3 3h10V1H3v2Z"}
         ) 
       ]
     )
---
+++ dist-bootstrap/icons/sign-intersection-t-fill.js
@@ -0,0 +1,22 @@
+import { h } from 'vue'
+export default {
+  $_icon: {
+    name: "SignIntersectionTFill",
+    vendor: "B",
+    license: "MIT",
+    type: [],
+    tags: ["sign","intersection","t","fill"],
+  },
+  render() {
+    return h(
+      "svg",
+      {"xmlns":"http://www.w3.org/2000/svg","width":"16","height":"16","fill":"currentColor","viewBox":"0 0 16 16"},
+      [ 
+        h(
+          "path",
+          {"d":"M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098L9.05.435ZM5 5h6v1.5H8.75V12h-1.5V6.5H5V5Z"}
+        ) 
+      ]
+    )
+  }
+}
