e5db40ccf5f49d449f0f36729379007200de55c0
maciejg-git <ender@localhost>
2022-12-02 21:17:41 +0100

    remove icon types from icon component

--- dist-vue/Icon.vue
+++ dist-vue/Icon.vue
@@ -1,22 +1,12 @@
 <template>
-  <component :is="iconTypes[iconType] || name || icon"></component>
+  <component :is="name || icon"></component>
 </template>
 
 <script>
-import { inject } from "vue";
-
 export default {
   props: {
     icon: { type: [String, Object], default: "" },
     name: { type: [String, Object], default: "" },
-    iconType: { type: String, default: "" },
-  },
-  setup() {
-    let iconTypes = inject("icon-types", {});
-
-    return {
-      iconTypes,
-    };
   },
 };
 </script>
